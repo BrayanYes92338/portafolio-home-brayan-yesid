fd973e51d6f2004fde6f69099872b685c3d7e9b2
BrayanYes92338 <145297925+BrayanYes92338@users.noreply.github.com>
2023-12-01 17:58:54 -0500

    mini update

--- Citas de Veterinaria/js/main.js
+++ Citas de Veterinaria/js/main.js
@@ -41,8 +41,46 @@ function bform() {
 
 }
 
+let tipoMascotas = [{ img: "./img/istockphoto-963071796-170667a.jpg", tipo: "Perro" },
+{ img: "./img/R.jpg", tipo: "Gato" },
+{ img: "./img/depositphotos_152522110-stock-illustration-cute-blue-bird-cartoon-flying.jpg", tipo: "Ave" },
+{ img: "./img/R(1).jpg", tipo: "Reptil" },
+{ img: "./img/OIP.jpg", tipo: "Otro" }];
+
+let mascotas = [];
+let indice = null;
+let oper = null;
+
 function formulario() {
     let namemas = document.getElementById("nombre-mascota").value;
+    let namedue = document.getElementById("nombre-dueño").value;
+    let numtel = document.getElementById("num-tel").value;
+    let tipo = document.getElementById("tipo-animal").value;
+    let fecha = document.getElementById("fecha").value;
+    let hora = document.getElementById("hora").value;
+    let descri = document.getElementById("descri").value;
+
+    if(oper === true){
+        mascotas[indice].nombreMascota = document.getElementById("nombre-mascota").value;
+        mascotas[indice].nombreDueño = document.getElementById("nombre-dueño").value;
+        mascotas[indice].telefono = document.getElementById("num-tel").value;
+        mascotas[indice].tipoAnimal = document.getElementById("tipo-animal").value;
+        mascotas[indice].fecha = document.getElementById("fecha").value;
+        mascotas[indice].hora = document.getElementById("hora").value;
+        mascotas[indice].sintomas = document.getElementById("descri").value;
+    }else {
+        mascotas.push({
+            nombreMascota: namemas,
+            nombreDueño: namedue,
+            telefono: numtel,
+            tipoAnimal: tipo,
+            fecha: fecha,
+            hora: hora,
+            sintomas: descri
+        })
+    }
+    console.log(mascotas);
+
 }
 
 
@@ -60,8 +98,10 @@ function alertas() {
     let fecha_actual = new Date();
     let fecha_select = new Date(document.getElementById("fecha").value);
     let hora_select = document.getElementById("hora").value;
-    let vhora =  parseInt(hora_select.split(":")[0], 10);
+    let vhora = parseInt(hora_select.split(":")[0], 10);
     let vmin = parseInt(hora_select.split(":")[1], 10);
+    const horaLimite = 17;
+    const minLimite = 45;
 
 
     boton.addEventListener("click", () => {
@@ -71,7 +111,7 @@ function alertas() {
     boton2.addEventListener("click", () => {
         modal2.classList.remove("active");
     })
-    
+
     closeicon.addEventListener("click", () => {
         modal.classList.remove("active");
     })
@@ -96,7 +136,7 @@ function alertas() {
             document.getElementById("alert").textContent = "";
             modal.classList.remove("active");
         }, 5000);
-    }else if (!texto.test(document.getElementById("nombre-mascota").value)) {
+    } else if (!texto.test(document.getElementById("nombre-mascota").value)) {
         document.getElementById("alert").textContent = "El campo nombre no puede tener numeros";
         modal.classList.add("active");
         setTimeout(() => {
@@ -111,7 +151,7 @@ function alertas() {
             document.getElementById("alert").textContent = "";
             modal.classList.remove("active");
         }, 5000);
-    }else if(!texto.test(document.getElementById("nombre-dueño").value)){
+    } else if (!texto.test(document.getElementById("nombre-dueño").value)) {
         document.getElementById("alert").textContent = "El campo nombre del dueño no puede tener numeros";
         modal.classList.add("active");
         setTimeout(() => {
@@ -134,7 +174,7 @@ function alertas() {
             modal.classList.remove("active");
         }, 5000);
 
-    } else if(document.getElementById("num-tel").value.length !=10){
+    } else if (document.getElementById("num-tel").value.length != 10) {
         document.getElementById("alert").textContent = "El campo telefono debe tener 10 digitos";
         modal.classList.add("active");
         setTimeout(() => {
@@ -142,8 +182,8 @@ function alertas() {
             modal.classList.remove("active");
         }, 5000);
 
-    
-    } else if(tipoa == ""){
+
+    } else if (tipoa == "") {
         document.getElementById("alert").textContent = "Debe seleccionar un tipo de animal";
         modal.classList.add("active");
         setTimeout(() => {
@@ -151,7 +191,7 @@ function alertas() {
             modal.classList.remove("active");
         }, 5000);
 
-    }else if(document.getElementById("fecha").value == ""){
+    } else if (document.getElementById("fecha").value == "") {
         document.getElementById("alert").textContent = "Debe seleccionar una fecha";
         modal.classList.add("active");
         setTimeout(() => {
@@ -159,14 +199,14 @@ function alertas() {
             modal.classList.remove("active");
         }, 5000);
 
-    }else if(fecha_select < fecha_actual){
+    } else if (fecha_select < fecha_actual) {
         document.getElementById("alert").textContent = "La fecha seleccionada no puede ser menor a la fecha actual";
         modal.classList.add("active");
         setTimeout(() => {
             document.getElementById("alert").textContent = "";
             modal.classList.remove("active");
         }, 5000);
-    }else if(document.getElementById("hora").value == ""){
+    } else if (document.getElementById("hora").value == "") {
         document.getElementById("alert").textContent = "Debe seleccionar una hora";
         modal.classList.add("active");
         setTimeout(() => {
@@ -174,7 +214,7 @@ function alertas() {
             modal.classList.remove("active");
         }, 5000);
 
-    }else if(vhora < 8 || vhora > 17 || vmin < 0 || vmin > 45){
+    } else if (vhora < 8 || vhora > horaLimite || (vhora === horaLimite && vmin > minLimite)) {
         document.getElementById("alert").textContent = "Haz seleccionado una hora fuera del horario de atencion (8:00 am - 5:45 pm)";
         modal.classList.add("active");
         setTimeout(() => {
@@ -182,7 +222,7 @@ function alertas() {
             modal.classList.remove("active");
         }, 5000);
 
-    }else if(document.getElementById("descri").value == ""){
+    } else if (document.getElementById("descri").value == "") {
         document.getElementById("alert").textContent = "El campo de Sintomas no puede estar vacio";
         modal.classList.add("active");
         setTimeout(() => {
