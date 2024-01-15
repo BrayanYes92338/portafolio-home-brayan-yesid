517394faf85264788eeb15ec328e48559c9ea95b
BrayanYes92338 <145297925+BrayanYes92338@users.noreply.github.com>
2024-01-14 19:43:13 -0500

    new update for formulary

--- Formulario/formulario.js
+++ Formulario/formulario.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+const boton = document.querySelector(".button");
+const contenedor = document.querySelector(".contenedor-cars");
+
+boton.addEventListener("click", ()=>{
+    contenedor.classList.add("active")
+})
+
+tarjetas();
+
+});
+
+
 let registros = [];
 let oper = null;
 let indice = null;
@@ -26,7 +39,7 @@ function formulario() {
         acepta = "No acepta";
     }
 
-    if (oper === true) {
+    if (oper === true ) {
         registros[indice].name = document.getElementById("name").value;
         registros[indice].lastname = document.getElementById("lastname").value;
         registros[indice].birthdate = document.getElementById("birthdate").value;
@@ -68,9 +81,11 @@ function formulario() {
 
     console.log(registros);
     document.getElementById("tabla").innerHTML = ""
-
+    document.getElementById("recipiente").innerHTML = ""
     tabla();
+    tarjetas();
     oper = false
+
 
 
 }
@@ -322,14 +337,14 @@ function tabla() {
             eliminar(index)
         });
         td1.textContent = item.name,
-        td2.textContent = item.lastname,
-        td3.textContent = item.birthdate,
-        td4.textContent = item.phone,
-        td5.textContent = item.email,
-        td6.textContent = item.documenttype,
-        td7.textContent = item.documentnumber,
-        td8.textContent = item.male ? "Masculino" : "Femenino",
-        td9.appendChild(editar)
+            td2.textContent = item.lastname,
+            td3.textContent = item.birthdate,
+            td4.textContent = item.phone,
+            td5.textContent = item.email,
+            td6.textContent = item.documenttype,
+            td7.textContent = item.documentnumber,
+            td8.textContent = item.male ? "Masculino" : "Femenino",
+            td9.appendChild(editar)
         td9.appendChild(borrar)
         tr.appendChild(td1)
         tr.appendChild(td2)
@@ -345,6 +360,125 @@ function tabla() {
 
     });
 
+
+   
+}
+
+function tarjetas(){
+    let fragment = document.createDocumentFragment()
+    registros.forEach((item, index) => {
+        let divcontenedor = document.createElement("div");
+        divcontenedor.classList.add("contenedor");
+        let divcuerpo = document.createElement("div");
+        divcuerpo.classList.add("cuerpo");
+        let dcont_nombre = document.createElement("div");
+        dcont_nombre.classList.add("cont-nombre", "cn");
+        let pnombre = document.createElement("p");
+        pnombre.textContent ="Nombre:";
+        pnombre.classList.add("titulo");
+        let prnombre = document.createElement("p");
+        prnombre.textContent = item.name;
+        let dcont_apellido = document.createElement("div");
+        dcont_apellido.classList.add("cont-apellido", "cn");
+        let pappelido = document.createElement("p");
+        pappelido.textContent ="Apellido:";
+        pappelido.classList.add("titulo");
+        let prapellido = document.createElement("p");
+        prapellido.textContent = item.lastname;
+        let dfecha_n= document.createElement("div");
+        dfecha_n.classList.add("cont-fecha-n", "cn");
+        let pfecha_n = document.createElement("p");
+        pfecha_n.textContent ="Fecha de nacimiento:";
+        pfecha_n.classList.add("titulo");
+        let prfecha_n = document.createElement("p");
+        prfecha_n.textContent = item.birthdate;
+        let dtelefono = document.createElement("div");
+        dtelefono.classList.add("cont-telefono", "cn");
+        let ptelefono = document.createElement("p");
+        ptelefono.textContent ="Telefono:";
+        ptelefono.classList.add("titulo");
+        let prtelefono = document.createElement("p");
+        prtelefono.textContent = item.phone;
+        let dcorreo = document.createElement("div");
+        dcorreo.classList.add("cont-correo", "cn");
+        let pcorreo = document.createElement("p");
+        pcorreo.textContent ="Correo:";
+        pcorreo.classList.add("titulo");
+        let prcorreo = document.createElement("p");
+        prcorreo.textContent = item.email;
+        let dtipo_doc = document.createElement("div");
+        dtipo_doc.classList.add("cont-tipo-doc", "cn");
+        let ptipo_doc = document.createElement("p");
+        ptipo_doc.textContent ="Tipo de documento:";
+        ptipo_doc.classList.add("titulo");
+        let prtipo_doc = document.createElement("p");
+        prtipo_doc.textContent = item.documenttype;
+        let dnum_doc = document.createElement("div");
+        dnum_doc.classList.add("cont-num-doc", "cn");
+        let pnum_doc = document.createElement("p");
+        pnum_doc.textContent ="Numero de documento:";
+        pnum_doc.classList.add("titulo");
+        let prnum_doc = document.createElement("p");
+        prnum_doc.textContent = item.documentnumber;
+        let dgenero = document.createElement("div");
+        dgenero.classList.add("cont-genero", "cn");
+        let pgenero = document.createElement("p");
+        pgenero.textContent ="Genero:";
+        pgenero.classList.add("titulo");
+        let prgenero = document.createElement("p");
+        prgenero.textContent = item.male ? "Masculino" : "Femenino";
+        let dbotones = document.createElement("div");
+        dbotones.classList.add("botones", "cn");
+        let editar = document.createElement("button");
+        editar.classList.add("btn-editar");
+        editar.addEventListener("click", () => {
+            edicion2(item, index)
+        });
+        let iconoeditar = document.createElement("i");
+        iconoeditar.classList.add("fas", "fa-edit");
+        let borrar = document.createElement("button");
+        borrar.classList.add("btn-borrar");
+        borrar.addEventListener("click", () => {
+            eliminar2(index)
+        });
+        let iconoborrar = document.createElement("i");
+        iconoborrar.classList.add("fas", "fa-trash-alt");
+
+        divcontenedor.appendChild(divcuerpo);
+        divcuerpo.appendChild(dcont_nombre);
+        dcont_nombre.appendChild(pnombre);
+        dcont_nombre.appendChild(prnombre);
+        divcuerpo.appendChild(dcont_apellido);
+        dcont_apellido.appendChild(pappelido);
+        dcont_apellido.appendChild(prapellido);
+        divcuerpo.appendChild(dfecha_n);
+        dfecha_n.appendChild(pfecha_n);
+        dfecha_n.appendChild(prfecha_n);
+        divcuerpo.appendChild(dtelefono);
+        dtelefono.appendChild(ptelefono);
+        dtelefono.appendChild(prtelefono);
+        divcuerpo.appendChild(dcorreo);
+        dcorreo.appendChild(pcorreo);
+        dcorreo.appendChild(prcorreo);
+        divcuerpo.appendChild(dtipo_doc);
+        dtipo_doc.appendChild(ptipo_doc);
+        dtipo_doc.appendChild(prtipo_doc);
+        divcuerpo.appendChild(dnum_doc);
+        dnum_doc.appendChild(pnum_doc);
+        dnum_doc.appendChild(prnum_doc);
+        divcuerpo.appendChild(dgenero);
+        dgenero.appendChild(pgenero);
+        dgenero.appendChild(prgenero);
+        divcuerpo.appendChild(dbotones);
+        dbotones.appendChild(editar);
+        editar.appendChild(iconoeditar);
+        dbotones.appendChild(borrar);
+        borrar.appendChild(iconoborrar);
+        fragment.appendChild(divcontenedor);
+
+
+    });
+    document.getElementById("recipiente").appendChild(fragment);
 }
 
 function edicion(r, i) {
@@ -364,10 +498,36 @@ function edicion(r, i) {
 
 }
 
-function eliminar(i){
-    index=i
-registros.splice(index,1);
-document.getElementById("tabla").innerHTML = "";
-tabla();
+function edicion2(r, i) {
+    oper = true
+    indice = i
+    console.log(r);
+    document.getElementById("name").value = r.name;
+    document.getElementById("lastname").value = r.lastname;
+    document.getElementById("birthdate").value = r.birthdate;
+    document.getElementById("phone").value = r.phone;
+    document.getElementById("email").value = r.email;
+    document.getElementById("documentType").value = r.documenttype;
+    document.getElementById("documentNumber").value = r.documentnumber;
+    document.getElementById("male").checked = r.male;
+    document.getElementById("female").checked = r.female;
+    document.getElementById("termsAndConditions").checked = r.termsAndConditions;
+
+}
+
+
+function eliminar(i) {
+    index = i
+    registros.splice(index, 1);
+    document.getElementById("tabla").innerHTML = "";
+    tabla();
+
+}
+
+function eliminar2(i) {
+    index = i
+    registros.splice(index, 1);
+    document.getElementById("recipiente").innerHTML = "";
+    tarjetas();
 
 }
